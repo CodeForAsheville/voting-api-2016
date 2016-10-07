9274201b1f99f0384f88563064731c28f81cc605
ejaxon <eric@deepweave.com>
2016-10-07 06:17:20 -0400

    Adding in the data file

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var pg = require('pg');
+var fs = require('fs');
+var parse = require('csv-parse');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -18,6 +20,16 @@ var Client = pg.Client;
 
 var client = new Client(process.env.DATABASE_URL);
 client.connect();
+
+app.get('/reload', function (request, response) {
+  fs.readFile('./ncvoter11.txt', 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("The length of the data is " + data.length);
+    console.log("And the first line is " + JSON.stringify(data[0]));
+  });
+});
 
 app.get('/db', function (request, response) {
 
